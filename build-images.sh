e2117438e3b648bc72cab5e713b299a95f744f06
Wang Haiqing <krman@163.com>
2019-11-04 10:41:44 +0800

    111

--- build-images.sh
+++ build-images.sh
@@ -5,6 +5,9 @@ docker login --username=krman_1@163.com registry.cn-hangzhou.aliyuncs.com
 
 docker tag webkubectl:latest registry.cn-hangzhou.aliyuncs.com/wanghaiqing/webkubectl:latest
 docker push registry.cn-hangzhou.aliyuncs.com/wanghaiqing/webkubectl:latest
+
+kubectl delete -f webkubectl.yaml
+kubectl apply  -f webkubectl.yaml
  
 exit 0
  
@@ -12,4 +15,4 @@ docker run -d --name="webkubectl" -p 38080:18080 --privileged registry.cn-hangzh
 
 http://103.45.112.173:38080/
 
-kubectl apply -f webkubectl.yaml
+
